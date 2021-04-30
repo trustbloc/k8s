0aa5e90b3b4371532ccdf144cb5714684ab8b649
Firas Qutishat <firas.qutishat@securekey.com>
2021-04-30 15:09:27 -0400

    chore: update to latest orb
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- orb/kustomize/orb/overlays/local/testnet/create_follow_activity.sh
+++ orb/kustomize/orb/overlays/local/testnet/create_follow_activity.sh
@@ -9,20 +9,34 @@ apk --no-cache add curl
 
 domain1IRI=https://orb-1.||DOMAIN||/services/orb
 domain2IRI=https://orb-2.||DOMAIN||/services/orb
-followID=1
-inviteWitnessID=2
+orb1FollowID=1
+orb2FollowID=2
+orb1InviteWitnessID=3
+orb2InviteWitnessID=4
 
-followActivity=$(curl -o /dev/null -s -w "%{http_code}" --header "Content-Type: application/json" \
+# orb2 server follows orb1 server
+followActivityORB2=$(curl -o /dev/null -s -w "%{http_code}" --header "Content-Type: application/json" \
    --request POST \
-   --data '{"@context":"https://www.w3.org/ns/activitystreams","id":"'$domain2IRI'/activities/'$followID'","type":"Follow","actor":"'$domain2IRI'","to":"'$domain1IRI'","object":"'$domain1IRI'"}' \
+   --data '{"@context":"https://www.w3.org/ns/activitystreams","id":"'$domain2IRI'/activities/'$orb2FollowID'","type":"Follow","actor":"'$domain2IRI'","to":"'$domain1IRI'","object":"'$domain1IRI'"}' \
    --insecure https://orb-1.||DOMAIN||/services/orb/inbox)
 
-
-inviteWitness=$(curl -o /dev/null -s -w "%{http_code}" --header "Content-Type: application/json" \
+# orb1 server follows orb2 server
+followActivityORB1=$(curl -o /dev/null -s -w "%{http_code}" --header "Content-Type: application/json" \
    --request POST \
-   --data '{"@context":["https://www.w3.org/ns/activitystreams","https://trustbloc.github.io/did-method-orb/contexts/anchor/v1"],"id":"'$domain1IRI'/activities/'$inviteWitnessID'","type":"InviteWitness","actor":"'$domain1IRI'","to":"'$domain2IRI'","object":"'$domain2IRI'"}' \
+   --data '{"@context":"https://www.w3.org/ns/activitystreams","id":"'$domain1IRI'/activities/'$orb1FollowID'","type":"Follow","actor":"'$domain1IRI'","to":"'$domain2IRI'","object":"'$domain2IRI'"}' \
    --insecure https://orb-2.||DOMAIN||/services/orb/inbox)
 
+# orb1 invites orb2 to be a witness
+inviteWitnessORB1=$(curl -o /dev/null -s -w "%{http_code}" --header "Content-Type: application/json" \
+   --request POST \
+   --data '{"@context":["https://www.w3.org/ns/activitystreams","https://trustbloc.github.io/did-method-orb/contexts/anchor/v1"],"id":"'$domain1IRI'/activities/'$orb1InviteWitnessID'","type":"InviteWitness","actor":"'$domain1IRI'","to":"'$domain2IRI'","object":"'$domain2IRI'"}' \
+   --insecure https://orb-2.||DOMAIN||/services/orb/inbox)
+
+# orb2 invites orb1 to be a witness
+inviteWitnessORB2=$(curl -o /dev/null -s -w "%{http_code}" --header "Content-Type: application/json" \
+   --request POST \
+   --data '{"@context":["https://www.w3.org/ns/activitystreams","https://trustbloc.github.io/did-method-orb/contexts/anchor/v1"],"id":"'$domain2IRI'/activities/'$orb2InviteWitnessID'","type":"InviteWitness","actor":"'$domain2IRI'","to":"'$domain1IRI'","object":"'$domain1IRI'"}' \
+   --insecure https://orb-1.||DOMAIN||/services/orb/inbox)
 
 
 checkIsCreated()
@@ -36,5 +50,7 @@ checkIsCreated()
    fi
 }
 
-checkIsCreated $followActivity follow-activity
-checkIsCreated $inviteWitness invite-witnessy
+checkIsCreated $followActivityORB2 follow-activity-orb2
+checkIsCreated $followActivityORB1 follow-activity-orb1
+checkIsCreated $inviteWitnessORB1 invite-witnessy-orb1
+checkIsCreated $inviteWitnessORB2 invite-witnessy-orb2
